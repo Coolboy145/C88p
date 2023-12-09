decf6fffe75ea21ce769560f8c50b4dcf43d29b4
Coolboy145 <88191598+Coolboy145@users.noreply.github.com>
2023-12-09 16:57:06 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA8UPuy66BxbG5OQKoskOVGsjGzBos6sCo",
+  authDomain: "c86c-d2688.firebaseapp.com",
+  projectId: "c86c-d2688",
+  storageBucket: "c86c-d2688.appspot.com",
+  messagingSenderId: "817077185340",
+  appId: "1:817077185340:web:5b941c96dd005677bacc39"
+};
